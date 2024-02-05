4eb311144f9014914a5e4ae0b4e40ae35b9f8a5e
Amal Gohar <amalxgohar@gmail.com>
2024-02-05 17:54:03 -0500

    Update colorwiz.py

--- colorwiz.py
+++ colorwiz.py
@@ -51,7 +51,7 @@ class DALLEApp: # Start DALLEEApp Class
         self.root = root
         root.geometry("550x740")
         root.resizable(True, True)
-        self.root.title("ColorWhiz: Coloring Book Generator")
+        self.root.title("ColorWiz: Coloring Book Generator")
         frame = customtkinter.CTkFrame(master=root, width=550,
                                        height=740, corner_radius=10,
                                        border_width=1)
